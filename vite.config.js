945043c9dc19cb03b82cf905c0126e6907c4c03c
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2026-04-28 18:37:10 -0400

    Switch from react-swc to regular react plugin for better compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // Plugin to exclude large files from build
 const excludeLargeFiles = () => ({
